7cc7b9eb6bd6a65b73d737b20ac06ad41ae29247
DanielJR11 <44812185+DanielJR11@users.noreply.github.com>
2020-01-28 23:57:04 -0400

    menu funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  {path: 'inicio',loadChildren: './pages/inicio/inicio.module#InicioPageModule'},
+  {
+    path: 'list',
+    loadChildren: () => import('./pages/list/list.module').then( m => m.ListPageModule)
+  },
+  {
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+  },
 ];
 
 @NgModule({
